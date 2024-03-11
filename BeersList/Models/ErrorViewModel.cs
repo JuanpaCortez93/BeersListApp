f991757bad1b0cbe671c0400c6c3d93f996f6ed9
Juan Pablo Cortez Mosquera <juanpacortexz93@gmail.com>
2024-03-11 17:48:30 -0500

    Agregar archivos de proyecto.

---
+++ BeersList/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeersList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
